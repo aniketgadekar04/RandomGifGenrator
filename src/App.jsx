db4f7de5e562230b5e9529a9ffcd64329182a79d
aniketgadekar04 <156211209+aniketgadekar04@users.noreply.github.com>
2026-02-03 11:20:37 +0530

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Random from "./components/Random";
+import Tag from "./components/Tag";
 
 function App() {
   return (
@@ -7,10 +9,10 @@ function App() {
         <h1 className="bg-white rounded-lg uppercase w-11/12 text-center mt-[40px] ml-[15px] py-2 px-10 text-4xl font-bold ">
           Random Gifs
         </h1>
-        {/* <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
           <Random />
           <Tag />
-        </div> */}
+        </div>
       </div>
     </>
   );
